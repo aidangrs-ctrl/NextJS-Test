2545d755de9392e808e6fd1a11e614d24f40c6cc
Aidan Green <aidangrs@amazon.com>
2024-07-25 14:11:26 -0700

    Adding a /500.js

---
+++ pages/500.js
@@ -0,0 +1,12 @@
+import Layout from "../components/layout";
+
+export default function Page() {
+  return (
+    <Layout>
+      <article>
+        <h1>500</h1>
+        <p>✅ Test passed. If you see this page, the pages/500.js page is working</p>
+      </article>
+    </Layout>
+  );
+}
