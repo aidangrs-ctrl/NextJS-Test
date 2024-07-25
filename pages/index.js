487fa8609de65fd1928e7e9fa835e436c547ab16
Aidan Green <aidangrs@amazon.com>
2024-07-25 13:30:48 -0700

    prefetch false ssrredir and ssg

--- pages/index.js
+++ pages/index.js
@@ -22,12 +22,12 @@ export default function Home({}) {
       </section>
       <hr />
       <section>
-        <Link href={"/ssg"}>Static Site Generation (SSG)</Link><br />
+        <Link href={"/ssg"} prefetch={false}>Static Site Generation (SSG)</Link><br />
         <Link href={"/ssg-dynamic/1"}>Static Site Generation — with dynamic routes</Link><br />
         <Link href={"/ssg-dynamic-fallback/1"}>Static Site Generation — with dynamic route fallback</Link><br />
         <Link href={`/isr`}>Incremental Static Regeneration (ISR)</Link><br />
         <Link href={`/ssr`}>Server Side Rendering (SSR)</Link><br />
-        <Link href={`/ssr-redirect`}>Server Side Rendering - Redirect</Link><br />
+        <Link href={`/ssr-redirect`} prefetch={false}>Server Side Rendering - Redirect</Link><br />
         <Link href={`/ssr-not-found`}>Server Side Rendering - Page not found</Link><br />
         <Link href={`/api-route`}>API Route</Link><br />
         <Link href={`/middleware-rewrite`}>Middleware — Rewrite</Link><br />
